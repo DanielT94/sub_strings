e3005ac76c8ffa76efc273ae88215f3f53143c33
DanielT94 <daniel.turnbull94@gmail.com>
2022-08-19 11:57:05 +0100

    Ran rubocop for formatting

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,13 +1,13 @@
 def substrings(string, dictionary)
-    result = Hash.new(0)
-    lowered_string = string.downcase
-    dictionary.each do |word|
-        matches = lowered_string.scan(word).length
-        result[word] = matches unless matches == 0
-    end
-    return result
+  result = Hash.new(0)
+  lowered_string = string.downcase
+  dictionary.each do |word|
+    matches = lowered_string.scan(word).length
+    result[word] = matches unless matches == 0
+  end
+  result
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
